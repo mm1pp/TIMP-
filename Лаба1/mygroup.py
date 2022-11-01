2e3504d7cc25600707cc22c59c9443fadcd1767b
mm1pp <117196238+mm1pp@users.noreply.github.com>
2022-11-01 19:27:49 +0300

    Первая лаба

---
+++ Лаба1/mygroup.py
@@ -0,0 +1,40 @@
+groupmates = [
+    {
+        "name": "Александр",
+        "surname": "Иванов",
+        "exams": ["Информатика", "ЭЭиС", "Web"],
+        "marks": [4, 3, 5]
+    },
+    {
+        "name": "Иван",
+        "surname": "Петров",
+        "exams": ["История", "АиГ", "КТП"],
+        "marks": [4, 4, 4]
+    },
+    {
+        "name": "Кирилл",
+        "surname": "Смирнов",
+        "exams": ["Философия", "ИС", "КТП"],
+        "marks": [5, 5, 5]
+    },
+    {
+        "name": "Владислав",
+        "surname": "Лимонов",
+        "exams": ["Математика", "ОИБ", "КТП"],
+        "marks": [2, 3, 5]
+    },
+    {
+        "name": "Никита",
+        "surname": "Сенаторов",
+        "exams": ["Русский", "ИС", "ТИМП"],
+        "marks": [5, 5, 4]
+    }
+]
+
+def print_students(students):
+    print(u"Имя".ljust(15), u"Фамилия".ljust(10), u"Экзамены".ljust(30), u"Оценки".ljust(20))
+    for student in students:
+        print(student["name"].ljust(15), student["surname"].ljust(10), str(student["exams"]).ljust(30), str(student["marks"]).ljust(20))
+
+print_students(groupmates)
+
